e4b19aa58b411910817116b14d1a68c01d88accf
Sao Channy <saochanny9944@gmail.com>
2026-02-05 15:31:28 +0700

    add new provider in enums

--- src/main/java/ecommerce_app/constant/enums/AuthProvider.java
+++ src/main/java/ecommerce_app/constant/enums/AuthProvider.java
@@ -8,9 +8,13 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public enum AuthProvider {
   LOCAL("local"),
+  EMAIL_PASSWORD("password"),
   GOOGLE("google.com"),
   GITHUB("github.com"),
-  FACEBOOK("facebook.com");
+  FACEBOOK("facebook.com"),
+  APPLE("apple.com"),
+  PHONE_NUMBER("phone"),
+  ANONYMOUS("anonymous");
   private final String provider;
 
   public static AuthProvider fromProviderString(String providerString) {
